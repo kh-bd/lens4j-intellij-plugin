15d7d2ed9d4b23bfc5c03f96f3b8417cb8933515
Sergei Khadanovich <khadanovich.sergey@gmail.com>
2022-11-03 11:42:49 +0700

    Notify about stale lens4j dependency one for all modules.

--- src/main/java/dev/khbd/lens4j/intellij/activity/CheckLens4jLatestVersionActivity.java
+++ src/main/java/dev/khbd/lens4j/intellij/activity/CheckLens4jLatestVersionActivity.java
@@ -13,10 +13,16 @@ import com.intellij.openapi.startup.StartupActivity;
 import dev.khbd.lens4j.intellij.Lens4jBundle;
 import dev.khbd.lens4j.intellij.common.Version;
 import dev.khbd.lens4j.intellij.notification.Lens4jNotificationGroup;
+import lombok.Value;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 /**
  * @author Sergei_Khadanovich
@@ -28,44 +34,61 @@ public class CheckLens4jLatestVersionActivity implements StartupActivity.DumbAwa
     @Override
     public void runActivity(Project project) {
         ModuleManager moduleManager = ModuleManager.getInstance(project);
-        for (Module module : moduleManager.getModules()) {
-            OrderEntry entry = findLens4jEntry(module);
-            if (Objects.nonNull(entry)) {
-                Matcher matcher = VERSION_PATTERN.matcher(entry.getPresentableName());
-                if (matcher.matches()) {
-                    checkVersion(project, module, Version.parse(matcher.group(2)));
-                }
-            }
-        }
+
+        Map<Version, List<Module>> groupedByVersion =
+                Arrays.stream(moduleManager.getModules())
+                        .map(module -> new ModuleAndLens4j(module, findLens4jVersion(module)))
+                        .filter(ModuleAndLens4j::isPresentStaleVersion)
+                        .collect(Collectors.groupingBy(ModuleAndLens4j::getVersion, Collectors.mapping(ModuleAndLens4j::getModule, Collectors.toList())));
+
+        groupedByVersion.forEach((version, modules) -> warnAboutStaleVersion(project, modules, version));
     }
 
-    private void checkVersion(Project project, Module module, Version version) {
-        if (version.compareTo(Version.LATEST) < 0) {
-            Notification notification = Lens4jNotificationGroup
-                    .getInstance()
-                    .createNotification(
-                            Lens4jBundle.getMessage("activity.check.version.title"),
-                            Lens4jBundle.getMessage("activity.check.version.message",
-                                    project.getName(),
-                                    module.getName(),
-                                    version,
-                                    Version.LATEST
-                            ),
-                            NotificationType.WARNING,
-                            NotificationListener.URL_OPENING_LISTENER
-                    );
-            Notifications.Bus.notify(notification, project);
-        }
+    private void warnAboutStaleVersion(Project project, List<Module> modules, Version version) {
+        List<String> names = modules.stream().map(Module::getName).collect(Collectors.toList());
+        Notification notification = Lens4jNotificationGroup
+                .getInstance()
+                .createNotification(
+                        Lens4jBundle.getMessage("activity.check.version.title"),
+                        Lens4jBundle.getMessage("activity.check.version.message",
+                                project.getName(),
+                                names,
+                                version,
+                                Version.LATEST
+                        ),
+                        NotificationType.WARNING,
+                        NotificationListener.URL_OPENING_LISTENER
+                );
+        Notifications.Bus.notify(notification, project);
     }
 
-    private OrderEntry findLens4jEntry(Module module) {
+    private Version findLens4jVersion(Module module) {
         ModuleRootManager rootManager = ModuleRootManager.getInstance(module);
-        for (OrderEntry entry : rootManager.getOrderEntries()) {
-            if (entry.getPresentableName().contains("lens4j")) {
-                return entry;
-            }
+        return Arrays.stream(rootManager.getOrderEntries())
+                .map(OrderEntry::getPresentableName)
+                .filter(name -> name.contains("lens4j"))
+                .findFirst()
+                .flatMap(this::parseVersion)
+                .orElse(null);
+    }
+
+    private Optional<Version> parseVersion(String lens4jName) {
+        Matcher matcher = VERSION_PATTERN.matcher(lens4jName);
+        if (matcher.matches()) {
+            return Optional.of(Version.parse(matcher.group(2)));
         }
-        return null;
+        return Optional.empty();
+    }
+
+    @Value
+    private static class ModuleAndLens4j {
+        Module module;
+        Version version;
+
+        boolean isPresentStaleVersion() {
+            return Objects.nonNull(version) && version.compareTo(Version.LATEST) < 0;
+        }
+
     }
 
 }
